a0ebe96e08fdaaa46245841b7e5d0ca600498037
IsmailBarkani <ismail.barkani@um5s.net.ma>
2020-03-18 17:14:00 +0100

    add genres

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../services/spotify.service';
+import { Artist } from '../../../Artist';
 
 @Component({
   selector: 'app-search',
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -5,7 +5,7 @@ import { HttpClient,HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class SpotifyService{
     searchUrl:string;
-    private access_token: string = 'BQCa3F2nmvB9GTM_eOAgVEKlYEV8a7B8RolxXLUI1Wo3n2_r9vHpV9mXfgDBYqutseKUKZwEF7F8KHn5Bnj3B9lJ4wtRPlgpACAXS2e2EYmU_lB96S_5CqzIjLjSPE-eVYnyHdOAEid3kScMXAEP1D-6hgKCUFd-TzzZet76mK7YjbMsGw2rQ5azmyLnE02bj8sTuThK3qI268sOROIFGj_qS0lzTfuSdcVk0F3i5TPfTxx0rKOSNHycwMz0pnNkot8nEUaiB32JF1f1FlDheEabcuH2Zd0NUg';
+    private access_token: string = 'BQB2yVOGPoBu6i8pfcuW6f9PrfEWet5xpnAYaSRkrekX25tgNgAr0-3AlTzGWY2w1ybOtUA5q_HYbaLWRwJCw-AKJ7Y8tC03SLnMilgCdmO4B9S5ACkVl67w0tjJVZym1Z6biQlSIwSpJmHYbnxyy1G7VRlpQkefNEix0f3g7QHwxz4l1lNeAx9x-6YVyCBu1N66dDRge_LhfLrZxDChJTI8XgbboYLqtXn9e5ifC0Qmr55wcCLtuLDz1GBr_OQna8cSLJ31eAHEkmx4WDavwFVCVdl0mzKglQ';
 
 
     constructor(private httpClient:HttpClient){
@@ -16,7 +16,7 @@ export class SpotifyService{
         let headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.access_token);
 
 
-        this.searchUrl = 'https://api.spotify.com/v1/search?query='+str+'&offset=0&limit=20&type='+type+'&market=US';
+        this.searchUrl = 'https://api.spotify.com/v1/search?query='+str+'&offset=0&limit=6&type='+type+'&market=US';
         return this.httpClient.get(this.searchUrl, {headers: headers });
         
     }
