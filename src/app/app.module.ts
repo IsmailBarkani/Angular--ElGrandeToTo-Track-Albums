952342c55650e00f646106d4af118693bcc0697c
IsmailBarkani <ismail.barkani@um5s.net.ma>
2020-03-18 15:21:21 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
 
+const appRouter:Routes = [
+  {path:'' , component:SearchComponent},
+  {path:'about', component:AboutComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRouter),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
